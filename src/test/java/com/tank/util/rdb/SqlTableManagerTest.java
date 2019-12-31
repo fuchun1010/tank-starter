86b0bbcd030224bcd7176df688af17db7458c578
fuchun198435163.com <tank198435@163.com>
2019-12-31 13:37:37 +0800

    [fuchun] #refactor

--- src/test/java/com/tank/util/rdb/SqlTableManagerTest.java
+++ src/test/java/com/tank/util/rdb/SqlTableManagerTest.java
@@ -16,7 +16,7 @@ public class SqlTableManagerTest {
     props.put("username", "root");
     props.put("password", "123");
     props.put("url", "jdbc:mysql://localhost:3307/order_crm");
-    Map<String, Object> data = this.sqlTableManager.createSqlTableScript("order_crm", props, 2);
+    Map<String, Object> data = this.sqlTableManager.createSqlTableScript("order_crm", props, 4);
     Assert.assertTrue(data.size() > 0);
     this.sqlTableManager.generateSql(data, "order_crm");
   }
