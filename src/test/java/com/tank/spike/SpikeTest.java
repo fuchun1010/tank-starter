a2ed45ecd7c4fe37823ce1abd7da8e7266ad61ba
fuchun198435163.com <tank198435@163.com>
2020-01-31 15:31:40 +0800

    [fuchun] #Tech only spike

--- src/test/java/com/tank/spike/SpikeTest.java
+++ src/test/java/com/tank/spike/SpikeTest.java
@@ -2,14 +2,36 @@ package com.tank.spike;
 
 import com.google.common.collect.Lists;
 import lombok.SneakyThrows;
+import lombok.val;
 import org.junit.Test;
 
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.Objects;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.ConcurrentLinkedDeque;
 
+import static java.lang.String.format;
+
 public class SpikeTest {
+
+  @Test
+  public void testDiff() {
+    DateTimeFormatter df = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+    val start = LocalDateTime.now();
+    val end = df.parse("2020-01-31 23:59:59");
+    long differWithMinutes = Duration.between(start, LocalDateTime.from(end)).toMinutes();
+
+    long round = differWithMinutes / 60;
+
+    long offset = differWithMinutes % 60;
+
+    System.out.println(format("round = %d, slot = %d", round, offset));
+
+  }
+
 
   @Test
   public void testEnd() {
@@ -17,7 +39,7 @@ public class SpikeTest {
     fields.add("user text");
     String delimiter = Character.toString((char) MESSAGE_DELIMITER);
     String end = Character.toString((char) MESSAGE_END);
-    String rs = String.format("%s%s", String.join(delimiter, fields), end);
+    String rs = format("%s%s", String.join(delimiter, fields), end);
     System.out.println(rs);
   }
 
